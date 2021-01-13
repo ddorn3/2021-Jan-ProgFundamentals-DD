452c72090a6c5b420cb93bef3d16fd7815eb4878
ddorn3 <77073151+ddorn3@users.noreply.github.com>
2021-01-13 12:08:11 -0700

    Practice console writeline - my first program "how to"

---
+++ FirstSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // the WriteLine() command will output the
+            //     the to the consle window.
+            Console.WriteLine("How to Withdraw Money from an ATM:");
+            Console.WriteLine("\t1)\tStand in front of the ATM then put appropriate debit card into the card slot");
+            Console.WriteLine("\t2)\tPress the appropriate onscreen account name from which the money is to be withdrawn");
+            Console.WriteLine("\t3)\tPress the onscreen numbers that make up your Personal Identification Number (PIN) and press ENTER");
+            Console.WriteLine("\t4)\tPress the onscreen WITHDRAW button");
+            Console.WriteLine("\t5)\tPress the onscreen numbers that make up the withdrawal amount and press ENTER");
+            Console.WriteLine("\t6)\tWait for the ATM to dispense the money then press NO OTHER TRANSACTIONS");
+            Console.WriteLine("\t7)\tRetrieve your cash from the cash dispensing tray and debit card from the card slot");
+            Console.WriteLine("Now go and spend all your cash on Lotto Max and don't forget to add Extra (a friend did that he and missed out on 80 grand");
+
+        }
+    }
+}
