dc8785e41f554659517ede0df1060412c7d19659
ddorn3 <77073151+ddorn3@users.noreply.github.com>
2021-01-30 23:31:21 -0700

    Feedback

---
+++ ClassPracEX-Jan27Solution/ClassPracEX-Jan27/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassPracEX_Jan27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
